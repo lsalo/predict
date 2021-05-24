22a7d78ebfcc93cd43b89273437ebd32e2464d5d
Lluis Salo <lsalo@mit.edu>
2021-05-24 13:44:34 -0400

    updated gridConvergence example (completed)

--- examples/gridConvergence.m
+++ examples/gridConvergence.m
@@ -18,29 +18,34 @@ mrstModule add mrst-gui coarsegrid upscaling incomp mpfa
 % Mandatory Input parameters
 %           {[FW], [HW]}
 name      = {'A', 'B', 'C', 'D', 'E'};
-thickness = {[repelem(10, 1, 10); repelem(10, 1, 10)], ...
-             [25 25 25 25; 25 25 25 25], [50 50; 50 50], ...
-             [20 10 20 10 30 10; 20 10 20 10 30 10], ...
-             [20 40 20 20; 40 20 20 20]};
-vcl       = {[repmat([0.2 0.6], 1, 5); repmat([0.5 0.3], 1, 5)], ...
-             [0.8 0.3 0.5 0; 0.3, 0.7, 0.15, 0.6], [0.5 0.1; 0.5 0.1], ...
-             [0.05 0.4 0.1 0.5 0.15 0.6; 0.2, 0.7, 0.25, 0.8, 0.3, 0.9], ...
-             [0 0.4 0.3 0.6; 0.1 0.4 0.2 0.6]};
-dip       = {[0, 0], [0, 0], [10, 20], [10, 5], [0, 0]};
-faultDip  = [45, 60, 65, 75, 55];
+thickness = {{repelem(10, 1, 10), repelem(10, 1, 10)}, ...
+             {[25 25 25 25], [25 25 25 25]}, ...
+             {[50 50], [50 50]}, ...
+             {[5 10 15 10 20 10 10 5 15], [20 10 20 10 30 10]}, ...
+             {[20 30 30 20]; [40 20 10 10 10 10]}};
+vcl       = {{repmat([0.2 0.6], 1, 5), repmat([0.5 0.3], 1, 5)}, ...
+             {[0.8 0.3 0.5 0], [0.3, 0.7, 0.15, 0.6]}, ...
+             {[0.5 0.1], [0.5 0.1]}, ...
+             {[0.3 0.6 0.1 0.7 0.2 0.8 0.3 0.9 0.1], [0.2, 0.7, 0.25, 0.8, 0.3, 0.9]}, ...
+             {[0 0.4 0.3 0.6], [0.1 0.4 0.2 0.6 0.1 0.5]}};
+dip       = {[0, 0], [0, 0], [10, 20], [5, -5], [0, 0]};
+faultDip  = [50, 60, 75, 85, 55];
 
 % Optional Input parameters
-nl   = [size(vcl{1}, 2), size(vcl{2}, 2), size(vcl{3}, 2), ...
-        size(vcl{4}, 2), size(vcl{5}, 2)];   % just for convenience here
+nl   = [numel(vcl{1}{1}), numel(vcl{1}{2}); ...
+        numel(vcl{2}{1}), numel(vcl{2}{2}); ...
+        numel(vcl{3}{1}), numel(vcl{3}{2}); ...
+        numel(vcl{4}{1}), numel(vcl{4}{2}); ...
+        numel(vcl{5}{1}), numel(vcl{5}{2})];  % just for convenience here
 zf   = {[100, 100], [500, 500], [1000, 1000], [100, 100], [2000, 2000]};    % m
-zmax = {[repelem(800, 1, nl(1)); repelem(800, 1, nl(1))], ...
-        [repelem(1000, 1, nl(2)); repelem(1000, 1, nl(2))], ...
-        [repelem(1000, 1, nl(3)); repelem(1000, 1, nl(3))], ...
-        [repelem(2000, 1, nl(4)); repelem(2000, 1, nl(4))], ...
-        [repelem(3000, 1, nl(5)); repelem(3000, 1, nl(5))]};
-cm = {'kao', 'sme', 'ill', 'mic', 'kao'};    % predominant clay mineral
-maxPerm = [];                   % cap max perm? [mD]
-siltInClay = [true, false, false, true, true];     % silt fraction in clay?
+zmax = {{repelem(800, 1, nl(1,1)), repelem(800, 1, nl(1,2))}, ...
+        {repelem(1000, 1, nl(2,1)), repelem(1000, 1, nl(2,2))}, ...
+        {repelem(1000, 1, nl(3,1)); repelem(1000, 1, nl(3,2))}, ...
+        {repelem(2000, 1, nl(4,1)); repelem(2000, 1, nl(4,2))}, ...
+        {repelem(3000, 1, nl(5,1)); repelem(3000, 1, nl(5,2))}};
+cm = {'kao', 'sme', 'ill', 'mic', 'kao'};   % predominant clay mineral
+maxPerm = 5000;                             % cap max perm? [mD]
+rho = 0.7;                                  % Corr. coeff. for multiv. distr.
 
 % Flow upscaling options
 U.useAcceleration = 1;          % 1 requires MEX and AMGCL setup
@@ -51,7 +56,7 @@ U.ARcheck         = 0;          % check if Perm obtained with grid with
                                 
 % Initialize target grid cell sizes and result containers
 Nstrat = numel(vcl);
-gridRes = repmat([1, 10; 0.5, 5; 0.2, 2; 0.1, 1; 0.05, 0.5; ...
+gridRes = repmat([0.5, 5; 0.2, 2; 0.1, 1; 0.05, 0.5; ...
                   0.02, 0.2; 0.01, 0.1], 1, 1, Nstrat);         
 Ngrid = size(gridRes, 1);
 poro  = zeros(Ngrid, Nstrat);
@@ -59,43 +64,46 @@ perm  = zeros(Ngrid, 3, Nstrat);
 cellDim = zeros(Ngrid, 2, Nstrat);
 
 tic
-for k=1:Nstrat
+parfor k=1:Nstrat
+faultDipIt = faultDip(k);
 % FW and HW
-footwall = Stratigraphy(thickness{k}(1,:), vcl{k}(1,:), dip{k}(1), ...
-                        'DepthFaulting', zf{k}(1), ...
-                        'DepthBurial', zmax{k}(1,:), 'ClayMine', cm{k});
-hangingwall = Stratigraphy(thickness{k}(2,:), vcl{k}(2,:), dip{k}(2), ...
-                           'IsHW', 1, 'NumLayersFW', footwall.NumLayers, ...
+footwall = Stratigraphy(thickness{k}{1}, vcl{k}{1}, ...
+                        'Dip', dip{k}(1), 'DepthFaulting', zf{k}(1), ...
+                        'DepthBurial', zmax{k}{1}, 'ClayMine', cm{k});
+hangingwall = Stratigraphy(thickness{k}{2}, vcl{k}{2}, ...
+                           'Dip', dip{k}(2), 'IsHW', 1, ...
+                           'NumLayersFW', footwall.NumLayers, ...
                            'DepthFaulting', zf{k}(2), ...
-                           'DepthBurial', zmax{k}(2,:), 'ClayMine', cm{k});
+                           'DepthBurial', zmax{k}{2}, 'ClayMine', cm{k});
 
 % Strati in Faulted Section
-mySect = FaultedSection(footwall, hangingwall);
+mySect = FaultedSection(footwall, hangingwall, faultDipIt, 'maxPerm', maxPerm);
+
+% Get material property distributions
+mySect = mySect.getMatPropDistr();
 
     % Run grid loop for each strati case
     for n=1:Ngrid
-    % Reset random seed (same values in MatProps and smear placement)
-    rng(5)
+    % Reset random seed (same values in MatProps and smear placement for
+    % successive grids with different sizes)
+    rng(10)
 
     % Generate fault object with properties for each realization
-    myFault = Fault(mySect, faultDip(k), gridRes(n, :, k));                 
+    myFault = Fault(mySect, faultDipIt, gridRes(n, :, k));                 
 
     % Get dependent variables
-    myFault = myFault.getMaterialProperties(mySect, 'maxPerm', maxPerm, ...
-                                            'siltInClay', siltInClay(k));   
+    myFault = myFault.getMaterialProperties(mySect, 'corrCoef', rho);   
 
     % Generate smear object with T, Tap, L, Lmax
-    Tap = getApparentThick(mySect, myFault.Dip);
-    smear = Smear(mySect.Vcl, mySect.IsClayVcl, mySect.Thick, Tap, ...
-                  mySect.DepthFaulting, myFault, 1, mySect);
+    smear = Smear(mySect, myFault, 1);
 
     % Compute upscaled permeability distribution
     myFault = myFault.upscaleSmearPerm(mySect, smear, U);
-
+    
     % Save result
     poro(n, k) = myFault.Poro;
     perm(n, :, k) = myFault.Perm;
-    cellDim(n, :, k) = myFault.Grid.CellDim;
+    cellDim(n, :, k) = myFault.Grid.cellDim;
     %disp(myFault.MatProps)
     disp(['Grid ' num2str(n) ' / ' num2str(Ngrid) ' completed.'])
     end
@@ -109,14 +117,15 @@ toc
 %% Output analysis
 
 % Plot permeabilities vs 1/h
-hL = cellDim(:,2);
-A  = cellDim(:,1).*cellDim(:,2);
+hL = cellDim(:,2,1);
+A  = cellDim(:,1,1).*cellDim(:,2,1);
 k_md = perm./(milli*darcy);
 
 % Plotting utilities
 sz = [14, 12];
 latx = {'Interpreter','latex'};
 %colrs = [128 0 0; 0 130 200; 255 225 128; 0 0 128; 0 0 0] ./ 255;
+idRes = 3;
 
 f1 = figure(1);
 tiledlayout(3, Nstrat, 'Padding', 'compact', 'TileSpacing', 'compact');
@@ -132,7 +141,7 @@ nexttile(k)
 hold on
 plot(1./hL, k_md(:,1,k), '-o', 'color', 'k', 'MarkerSize', 4, ...
      'DisplayName', name{k})
-plot(1/hL(4), k_md(4,1,k), '-o', 'color', 'k', 'MarkerFaceColor', ...
+plot(1/hL(idRes), k_md(idRes,1,k), '-o', 'color', 'k', 'MarkerFaceColor', ...
      [0.5 0.5 0.5], 'MarkerSize', 6, 'HandleVisibility','off')
 hold off
 grid on
@@ -154,7 +163,7 @@ title(name{k}, latx{:}, 'fontSize', sz(2));
 nexttile(k + Nstrat)
 hold on
 plot(1./hL, k_md(:,2,k), '-o', 'color','r', 'MarkerSize', 4)
-plot(1/hL(4), k_md(4,2,k), '-o', 'color', 'r', ...
+plot(1/hL(idRes), k_md(idRes,2,k), '-o', 'color', 'r', ...
      'MarkerFaceColor', [255, 125, 125]/255, 'MarkerSize', 6)
 hold off
 grid on
@@ -173,7 +182,7 @@ yticks(10.^(limy(2, 1):limy(2, 2)))
 nexttile(k + 2*Nstrat)
 hold on
 plot(1./hL, k_md(:,3,k), '-o', 'color', 'b', 'MarkerSize', 4)
-plot(1/hL(4), k_md(4,3,k), '-o', 'color', 'b', ...
+plot(1/hL(idRes), k_md(idRes,3,k), '-o', 'color', 'b', ...
      'MarkerFaceColor', [125, 125, 255]/255, 'MarkerSize', 6)
 hold off
 grid on
